43cab188b8234b6275aa09aa703f5f50a4f620ac
nihadkuluzade <nihadquluzade2001@gmail.com>
2024-08-29 09:13:46 +0400

    task

---
+++ js.js
@@ -0,0 +1,13 @@
+let admin;
+let name = "john"
+admin = name;
+alert(admin);
+console.log(admin);
+
+let planet = "earth";
+let visitor = "flankes"
+
+let a = 2;
+let x = 1 + (a *= 2);
+console.log("salam");
+
